7915d7ab010afd8828ddb6a6b2505a81a25cff33
Michael Nahum <USGrant63@gmail.com>
2016-06-02 18:31:14 -0400

    makes no sense. none.

--- script.js
+++ script.js
@@ -16,12 +16,12 @@ app.engine(".hbs", hbs({
   partialsDir:   "views",
   defaultLayout: "index"
 }));
+app.use("/assets", express.static('assets'));
 
 app.get("/", function(req, res){
-  res.render("index");
-});
-
-app.get("/results", function(req, res){
+  if (placeList.length > 0) {
+    placeList = []
+  }
   var url = "http://api.geonames.org/childrenJSON?lang=ar&geonameId=170062&username=hotspotsm"
   rp(url)
   .then(function(data){
@@ -39,24 +39,64 @@ app.get("/results", function(req, res){
   }).then(function() {
     placeList.forEach(function(place) {
       var params = {
-          q: place.name + ' since:2016-05-31',                                              //since yesterday?
-          lang: "ar",
-          count: 10
+        q: place.name + ' since:2016-05-31',                                              //since yesterday?
+        lang: "ar",
+        count: 10
+      }
+      twitterCall
+      .get('search/tweets', params)
+      .then(function (response) {
+        if (response.data.statuses != []) {
+          place.num_mentions = response.data.statuses.length
         }
-     twitterCall
-     .get('search/tweets', params)
-     .then(function (response) {
-       if (response.data.statuses != []) {
-         place.num_mentions = response.data.statuses.length
-       }
-       console.log(place);
-       })
+      })
     })
   })
-  res.render("results", {results: placeList});
+  res.render("index");
+});
+
+app.get("/results", function(req, res){
+  res.json(placeList)
 });
 
 
 app.listen(3001, function(){
   console.log("------------it's aliiive!!!------------");
 });
+
+
+
+// function getData() {
+//   var placeList = []
+//   var results;
+//   var url = "http://api.geonames.org/childrenJSON?lang=ar&geonameId=170062&username=hotspotsm"
+//   rp(url)
+//   .then(function(data){
+//     var parsed = JSON.parse(data)
+//     for(var i=0; i < parsed.geonames.length; i++){
+//       placeList.push(
+//         {
+//           name: parsed.geonames[i].name,
+//           lat:  parsed.geonames[i].lat,
+//           lng:  parsed.geonames[i].lng,
+//           num_mentions: 0
+//         }
+//       );
+//     }
+//   }).then(function() {
+//     placeList.forEach(function(place) {
+//       var params = {
+//         q: place.name + ' since:2016-05-31',                                              //since yesterday?
+//         lang: "ar",
+//         count: 10
+//       }
+//       twitterCall
+//       .get('search/tweets', params)
+//       .then(function (response) {
+//         if (response.data.statuses != []) {
+//           place.num_mentions = response.data.statuses.length
+//         }
+//       })
+//     })
+//   })
+// }

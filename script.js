6b6864c153e776c1740133f92f77da628a200163
Michael Nahum <USGrant63@gmail.com>
2016-06-02 21:45:49 -0400

    removed latlng bug, relocated twitterCall to server startup

--- script.js
+++ script.js
@@ -18,85 +18,59 @@ app.engine(".hbs", hbs({
 }));
 app.use("/assets", express.static('assets'));
 
-app.get("/", function(req, res){
-  if (placeList.length > 0) {
-    placeList = []
-  }
-  var url = "http://api.geonames.org/childrenJSON?lang=ar&geonameId=170062&username=hotspotsm"
-  rp(url)
-  .then(function(data){
-    var parsed = JSON.parse(data)
-    for(var i=0; i < parsed.geonames.length; i++){
-      placeList.push(
-        {
-          name: parsed.geonames[i].name,
-          lat:  parsed.geonames[i].lat,
-          lng:  parsed.geonames[i].lng,
-          num_mentions: 0
-        }
-      );
-    }
-  }).then(function() {
-    placeList.forEach(function(place) {
-      var params = {
-        q: place.name + ' since:2016-05-31',                                              //since yesterday?
-        lang: "ar",
-        count: 10
+// Never try to get external data from one of your node request handlers.
+// That response is asynchronous, and hard to coordinate. Also, every single time you refresh the page it'll
+// make a call to twitter, which is waaaaay too much.
+
+// So instead we'll move that request to another function.
+
+placeList = [];
+function get_tweets() {
+    if (placeList.length > 0) return;
+
+    var url = "http://api.geonames.org/childrenJSON?lang=ar&geonameId=170063&username=hotspotsm"
+    rp(url)
+    .then(function(data){
+      var parsed = JSON.parse(data)
+      for(var i=0; i < parsed.geonames.length; i++){
+        placeList.push(
+          {
+            name: parsed.geonames[i].name,
+            lat:  parsed.geonames[i].lat,
+            lng:  parsed.geonames[i].lng,
+            num_mentions: 0
+          }
+        );
       }
-      twitterCall
-      .get('search/tweets', params)
-      .then(function (response) {
-        if (response.data.statuses != []) {
-          place.num_mentions = response.data.statuses.length
+    }).then(function() {
+      placeList.forEach(function(place) {
+        var params = {
+          q: place.name + ' since:2016-05-31',                                              //since yesterday?
+          lang: "ar",
+          count: 10
         }
+        twitterCall
+        .get('search/tweets', params)
+        .then(function (response) {
+          if (response.data.statuses && response.data.statuses != []) {
+            place.num_mentions = response.data.statuses.length
+          }
+        })
       })
     })
-  })
+}
+
+get_tweets(); // this will happen as soon as the server starts up
+
+app.get("/", function(req, res){
   res.render("index");
 });
 
 app.get("/results", function(req, res){
-  res.json(placeList)
+  res.json(placeList);
 });
 
 
 app.listen(3001, function(){
   console.log("------------it's aliiive!!!------------");
 });
-
-
-
-// function getData() {
-//   var placeList = []
-//   var results;
-//   var url = "http://api.geonames.org/childrenJSON?lang=ar&geonameId=170062&username=hotspotsm"
-//   rp(url)
-//   .then(function(data){
-//     var parsed = JSON.parse(data)
-//     for(var i=0; i < parsed.geonames.length; i++){
-//       placeList.push(
-//         {
-//           name: parsed.geonames[i].name,
-//           lat:  parsed.geonames[i].lat,
-//           lng:  parsed.geonames[i].lng,
-//           num_mentions: 0
-//         }
-//       );
-//     }
-//   }).then(function() {
-//     placeList.forEach(function(place) {
-//       var params = {
-//         q: place.name + ' since:2016-05-31',                                              //since yesterday?
-//         lang: "ar",
-//         count: 10
-//       }
-//       twitterCall
-//       .get('search/tweets', params)
-//       .then(function (response) {
-//         if (response.data.statuses != []) {
-//           place.num_mentions = response.data.statuses.length
-//         }
-//       })
-//     })
-//   })
-// }
